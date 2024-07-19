c496650134693b742650b9cce1f30ad3367f7293
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-07-19 19:15:12 +0530

    api integrated

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const cors = require("cors")
-const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv")
+dotenv.config()
+const PORT = process.env.PORT || 4000;
 app.use(cors()) 
 app.get('/', async (req, res) => {
     try {
@@ -10,7 +12,7 @@ app.get('/', async (req, res) => {
             params: {
                 lat: 11.0645301,
                 lon: 76.0240333,
-                appid: 'ae813b7e4adcfad9ea2a1015f4d49ea1'
+                appid: process.env.api_key
             }
         });
         console.log(response.data); // Logging the response data for debugging
